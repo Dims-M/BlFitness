34782d36c7b12171e9b3cad731483b11b5b5f5f5
Дмитрий <rabolan@mail.ru>
2019-03-06 18:01:27 +0300

    1 урок

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Класс отвечающий за пол
+    /// </summary>
+   public class Gender
+    {
+        /// <summary>
+        /// Имя, называние
+        /// </summary>
+        public string Name { get; }
+
+        /// конструктор
+        /// <summary>
+        /// Конструктор при создании обьекта класса.
+        /// </summary>
+        public Gender(string name)
+        {
+            // проверка на кокектность входящих данных
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                //Вызываем искучение о ошибке в ручную
+                throw new ArgumentNullException("Имя пола не может быть пустым", nameof(name));
+            }
+
+            Name = name;
+            
+        }
+
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- CodeBlogFitness.BL/Model/User.cs
+++ CodeBlogFitness.BL/Model/User.cs
@@ -2,9 +2,24 @@
 
 namespace CodeBlogFitness.BL.Model
 {
-
+    /// <summary>
+    /// Класс описывающий пользователя в программе.
+    /// </summary>
     class User
     {
+        public string Name { get;} //Только получаем свойства. Изменять не можем. Усанавливаем при создании экземпляра класса
 
+        public Gender Cender { get; } // Пол пользователя.  Является классом со своими свойствами
+
+        public DateTime BirthDate { get; } // Дата рождения
+
+        public double Weight { get; set; } // Вес пользователя
+
+        public double Height { get; set; } // рост
+
+        public User(string name, Gender gender, DateTime birthDate,double weight, double height)
+        {
+            Name = name;
+        }
     }
 }
