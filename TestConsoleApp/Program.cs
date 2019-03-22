68132765d934db30ae63332e00fa9c7e1a27c721
Дмитрий <rabolan@mail.ru>
2019-03-22 18:38:14 +0300

    Начало работы с Linq

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -11,13 +11,27 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
+            var temColection = zapolneneRandomList(100);
             // zapolneneRandomList();
 
-            //VovodList(zapolneneRandomList(100));
+            // VovodList(zapolneneRandomList(10)); // вывод заполненого рандомом листа
 
-            testChenieFaila();
+            Console.WriteLine(ZaprosLino1(temColection));
+
+           // testChenieFaila(); // чтение из файлов.
 
             Console.ReadKey();
+        }
+
+
+        public static string ZaprosLino1(List<int> collections)
+        {
+            // запрос Linq. Выбрать в обьект  item из колекции collections. Где item < 5. Выбрать обьект item и присвоить его в переменную
+            var result = from item in collections
+                         where item < 5
+                         select item;
+
+            return result.ToString();
         }
 
         /// <summary>
@@ -51,7 +65,7 @@ namespace TestConsoleApp
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="list"></param>
-        public static void VovodList<T>(List<T> list)
+        public static void VovodList(List<int> list)
         {
             Console.WriteLine("Содержимое Листа");
             Console.WriteLine("Размер " + list.Count);
@@ -66,7 +80,7 @@ namespace TestConsoleApp
         }
 
         /// <summary>
-        /// Чтение из файла
+        /// Чтение из файла. С настрйкой пути файла 
         /// </summary>
         public static void testChenieFaila()
         {
@@ -90,25 +104,29 @@ namespace TestConsoleApp
                 Console.WriteLine("Размер: {0}", fileInf.Length);
                 countList = (int)fileInf.Length;
             }
-
-            //временный массив
+            //временный массив. 
             string [] strcollectionsing = new string [countList]; // 
 
-            using (StreamReader sr = new StreamReader(path))
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
             {
                 string line;
 
                 while ((line = sr.ReadLine()) != null) // читает поток пока не дайдет до конца файла
                 {
-                   string[] str = line.Split(' '); // читаем файл. и разбиваем его на цельные слова
+                   string temp = line.Trim();
+                   string[] str = temp.Split(' '); // читаем файл. и разбиваем его на цельные слова
 
-                    for (int i = 0; i < collections.Count; i++)
-                    {
-                        // collections.Insert(i, str);
+                   //if(str.Equals(" ") || str != null )
+                   // {  
                         collections.AddRange(str);
-                        Console.WriteLine($"Добавленно: {str}"); // Ятестовой вывод
-                    }
+                    Console.WriteLine($"Добавленно: {str}"); //  тестовой вывод
+
                 }
+
+
+              //  Console.WriteLine($"Добавленно: {str}"); //  тестовой вывод
+                   
+                
 
             }
 
