7c837ce685199a6dc723a11b9ce9e012d43c09ac
Дмитрий <rabolan@mail.ru>
2019-03-22 12:25:20 +0300

    чтение файлов

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,11 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            zapolneneRandomList();
+            // zapolneneRandomList();
 
-            VovodList(zapolneneRandomList());
+            //VovodList(zapolneneRandomList(100));
+
+            testChenieFaila();
 
             Console.ReadKey();
         }
@@ -20,16 +23,17 @@ namespace TestConsoleApp
         /// <summary>
         /// Рандомное заполнение листа случайными значениями. 
         /// </summary>
-        /// <param name="list">Конкретнвц тип колекции</param>
+        /// <param name="list">Конкретный тип колекции</param>
         /// <returns></returns>
-        public static List<int> zapolneneRandomList()
+        public static List<int> zapolneneRandomList(int dlinaLista)
         {
-            var collection = new List<int>();
+            
+            var collection = new List<int>(dlinaLista);
 
             Console.WriteLine("Случайное заполнение Array листа ");
            // Random random = new Random();
 
-            for (int i = 0; i < collection.Count; i++ )
+            for (int i = 0; i < collection.Capacity; i++ )
             {
                 Random random = new Random();
                 var teempp = random.Next(0, 1000);
@@ -50,16 +54,71 @@ namespace TestConsoleApp
         public static void VovodList<T>(List<T> list)
         {
             Console.WriteLine("Содержимое Листа");
-            Console.WriteLine("Размер" + list.Count);
+            Console.WriteLine("Размер " + list.Count);
 
             foreach (var LestTemp in list)
             {
                 Console.WriteLine(LestTemp.ToString());
-                Console.WriteLine("Размер"+ list.Count);
-                Console.WriteLine("\t\n");
+               // Console.WriteLine("Размер "+ list.Count);
+               // Console.WriteLine("\t\n");
             }
             Console.ReadKey();
         }
+
+        /// <summary>
+        /// Чтение из файла
+        /// </summary>
+        public static void testChenieFaila()
+        {
+            Console.WriteLine("чтение из файла");
+            //путь к файлу
+            string path = @"C:\Users\Dmitrii\Desktop\гг.txt";
+            int countList =0;
+
+            //лист для хранения спарсенных данных
+            var collections = new List<string>();
+
+           //Получаем данные о файле 
+            FileInfo fileInf = new FileInfo(path);
+
+            // если файл существует.
+            //Вывод инфы о файле
+            if (fileInf.Exists)
+            {
+                Console.WriteLine("Имя файла: {0}", fileInf.Name);
+                Console.WriteLine("Время создания: {0}", fileInf.CreationTime);
+                Console.WriteLine("Размер: {0}", fileInf.Length);
+                countList = (int)fileInf.Length;
+            }
+
+            //временный массив
+            string [] strcollectionsing = new string [countList]; // 
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null) // читает поток пока не дайдет до конца файла
+                {
+                   string[] str = line.Split(' '); // читаем файл. и разбиваем его на цельные слова
+
+                    for (int i = 0; i < collections.Count; i++)
+                    {
+                        // collections.Insert(i, str);
+                        collections.AddRange(str);
+                        Console.WriteLine($"Добавленно: {str}"); // Ятестовой вывод
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Количество элементов в коллекции " + collections.Count);
+            for (int i = 0; i < collections.Count; i++)
+            {
+                Console.WriteLine("Элемент [" + i + "]: " + collections[i]);
+            }
+        }
+
 
     }
 }
