a6a06c7bc9f1cf8b09d2d537e484924ad83bc42c
Дмитрий <rabolan@mail.ru>
2019-04-04 17:53:42 +0300

    разширение

---
+++ TestConsolle2/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace TestConsolle2
+{
+
+    /// <summary>
+    /// Класс для методов разширений
+    /// </summary>
+  public static class Helper
+    {
+        /// <summary>
+        /// Проверяем является ли число целым. Может ли оно иметь остаток от деления.Возращает булевское значение.
+        /// </summary>
+        /// <param name="value">Число</param>
+        /// <returns></returns>
+      public  static bool IsEven(this int value) // c помощ this мы явно укажем что для всех методов нужно использовать интовые параметры
+        {
+            Console.WriteLine("Проверяем  остаток на деление.");
+
+            return value % 2 == 0; // проверяем  остаток на деление. 
+        }
+
+        /// <summary>
+        /// Остается ли отстаток при делении 2 чисел.
+        /// </summary>
+        /// <param name="i">Число</param>
+        /// <param name="j">Делим на число</param>
+        /// <returns></returns>
+        public static bool isDivided( this int i, int j)
+        {
+            Console.WriteLine("Проверяем  остаток на деление.");
+            return i % j == 0; // проверяем  делится на остаток
+        }
+
+    }
+}
--- TestConsolle2/Program.cs
+++ TestConsolle2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Helper;
 
 namespace TestConsolle2
 {
@@ -15,7 +16,50 @@ namespace TestConsolle2
         static void Main(string[] args)
         {
 
+            Zapuskator();
         }
+
+
+        static void Zapuskator()
+        {
+            Console.WriteLine("ВВедите число");
+            var tempint = Console.ReadLine();
+
+            if (int.TryParse(tempint, out int result)) // в переменную result сохраняем спарсированное число из строки tempint 
+            {
+                // получаем булевый тим
+                // var isEvenn =  IsEven(result);
+                // var isEvenn = result.IsEven();
+
+                //Вызываем булевский метод .IsEven() параметром является значение в переменной result
+                if (result.IsEven()) // метод с помощью разширения. 
+                {
+                    Console.WriteLine($"Текущие значение {result} является остатком от деления");
+                }
+                else
+                {
+                    Console.WriteLine("Число не явдяется целым числом");
+                }
+                 
+            }
+
+            else
+            {
+                Console.WriteLine("Не корректные значения");
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Проверяем на четность и нечетность числа
+        /// </summary>
+        /// <param name="i"></param>
+        ///// <returns></returns>
+        //    static bool IsEven(  int i)
+        //    {
+        //        Console.WriteLine("Проверяем  остаток на деление.");
+            
+        //        return i % 2 == 0; // проверяем  остаток на деление. 
+        //    }
 
     }
 }
