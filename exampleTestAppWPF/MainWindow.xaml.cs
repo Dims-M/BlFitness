145c0b378b753aadda320a1ba576378279f54d8b
Дмитрий <rabolan@mail.ru>
2019-04-15 16:33:49 +0300

    перед doc panel

--- exampleTestAppWPF/MainWindow.xaml.cs
+++ exampleTestAppWPF/MainWindow.xaml.cs
@@ -31,34 +31,6 @@ namespace exampleTestAppWPF
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen; //устанавливаем локацию запуска программы
         }
 
-       //Кнопка 
-        private void BtnMessageBox_Click(object sender, RoutedEventArgs e)
-        {
-          
-            string temp = textl.Text;
-            MessageBox.Show("Доб окно");
-            MessageBox.Show(temp);
-
-        }
-
-        private void GetInput_Click(object sender, RoutedEventArgs e)
-        {
-           // var myProsses = new Process();
-
-
-            string dateImputUser = textl.Text.Trim();
-
-            if (dateImputUser !="" || dateImputUser != null|| dateImputUser != "." )
-            {
-                MessageBox.Show($"Были введены текущие данные: \t\n{dateImputUser}");
-                //MessageBox.Show("Не коректные данные");
-            }
-            //MessageBox.Show($"Были введены текущие данные: \t\n{dateImputUser}");
-            // MessageBox.Show($"Имя машины: \t\n{myProsses.MachineName}");
-            else
-            { 
-            MessageBox.Show("Не коректные данные");
-            }
-        }
+      
     }
 }
